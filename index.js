730b064eba10b0de74b0812ecaa00f8c0152817d
sbalayan1 <balayans2014@yahoo.com>
2021-05-20 21:57:35 -0700

    commit tree b2213811649ccfe52bee21293ceb547306af818c

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+        //event should change the text on the page to 'This is really cool!'
+        document.querySelector('p').textContent = 'This is really cool!'
+      });
+      
+
+console.log(
+  "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+);     
